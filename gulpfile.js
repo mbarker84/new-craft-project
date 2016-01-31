aa92a94441653fc86386f42fbc7c474fd099b52d
mbarker84 <mbarker_84@hotmail.co.uk>
2016-01-31 13:15:16 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('hello', function() {
+  console.log('Hello');
+});
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+})
+
+gulp.task('default', ['watch', 'sass', 'hello']);
